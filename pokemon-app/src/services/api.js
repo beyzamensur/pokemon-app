6e71fdebdf5caaaf35cba2612007ee5a12c93339
beyzamensur <117829487+beyzamensur@users.noreply.github.com>
2026-01-29 18:22:56 +0300

    Refactor getPokemonImageUrl by removing comments
    Removed comments regarding special handling for Nidoran images and clarified the image source for all Pokémon.

--- pokemon-app/src/services/api.js
+++ pokemon-app/src/services/api.js
@@ -16,15 +16,13 @@ export const getPokemonDetail = async (name) => {
 export const getPokemonImageUrl = (name) => {
   let pokemonName = name.toLowerCase();
 
-  // STAJ GÖREVİ ÖZEL ÇÖZÜMÜ: 
-  // Nidoranlar GIF olarak açılmadığı için onları farklı bir kaynaktan foto olarak çekiyoruz
+
   if (pokemonName === "nidoran-f" || pokemonName === "nidoran-m") {
-    // Nidoranlar için güvenilir statik (PNG) fotoğraf linki
+   
     return `https://img.pokemondb.net/sprites/home/normal/${pokemonName}.png`;
   }
 
-  // Diğer tüm Pokémonlar için hareketli GIF linki
-  // mr-mime gibi özel isimleri düzeltelim ki onlar da gitmesin
+
   let cleanName = pokemonName.replace('-normal', '').replace('mr-mime', 'mr.mime');
   
   return `https://projectpokemon.org/images/normal-sprite/${cleanName}.gif`;
